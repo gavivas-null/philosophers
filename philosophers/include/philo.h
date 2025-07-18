51283b4f5db9b97392b8c30e90ac9a3c26c61f95
Gabriel Jesus Vivas Perez <gavivas-@c1r9s3.42madrid.com>
2025-07-18 22:07:21 +0200

    se creó la estructura inicial

---
+++ philosophers/include/philo.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gavivas- <gavivas-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/18 19:42:43 by gavivas-          #+#    #+#             */
+/*   Updated: 2025/07/18 19:49:33 by gavivas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+
+
+#endif
