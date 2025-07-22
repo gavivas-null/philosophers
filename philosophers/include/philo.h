722f2e3c505bc9480f196c76b2d130fa9857e381
Gabriel Jesus Vivas Perez <gavivas-@c1r13s3.42madrid.com>
2025-07-22 20:10:12 +0200

    Se crearon las structs para el philo

--- philosophers/include/philo.h
+++ philosophers/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: gavivas- <gavivas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 19:42:43 by gavivas-          #+#    #+#             */
-/*   Updated: 2025/07/22 19:53:20 by gavivas-         ###   ########.fr       */
+/*   Updated: 2025/07/22 20:09:30 by gavivas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,30 @@
 # include <pthread.h>
 # include <sys/types.h>
 # include <sys/wait.h>
+
+typedef struct s_data
+{
+	int				n_philos;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				must_eat;
+	long			start_time;
+	int				stop_simulation;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	data_mutex;
+}				t_data;
+
+typedef struct t_philo
+{
+	int				id;
+	int				left_fork;
+	int				right_fork;
+	int				meal_count;
+	long			last_meal_time;
+	t_data			*data;
+	pthread_t		thread;
+}				t_philo;
 
 #endif
