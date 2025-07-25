d546e5d57ae829bda0515726aad321c324ef891a
Gabriel Jesus Vivas Perez <gavivas-@c1r6s3.42madrid.com>
2025-07-25 21:15:09 +0200

    Los filosofos piensan.

--- philosophers/src/routine.c
+++ philosophers/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: gavivas- <gavivas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 18:33:43 by gavivas-          #+#    #+#             */
-/*   Updated: 2025/07/25 18:53:22 by gavivas-         ###   ########.fr       */
+/*   Updated: 2025/07/25 21:13:08 by gavivas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*routine(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
-	printf("🧵 Filósofo %d ha comenzado su hilo.\n", philo->id);
+	printf("🧠 Filósofo %d está pensando\n", philo->id);
 	smart_sleep(200, philo->data);
 	return (NULL);
 }
