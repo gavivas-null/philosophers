1c791f03dccba6580ae095bd9283cf111a26e501
Gabriel Jesus Vivas Perez <gavivas-@c1r9s2.42madrid.com>
2025-08-25 19:05:45 +0200

    Proyecto modificado, actualmente falla el test 09 y el test 13.

--- philosophers/tester.sh
+++ philosophers/tester.sh
@@ -19,31 +19,31 @@ run_awk_check() {
 }
 
 echo "${BROWN}Test 01: 2 800 200 200 10${RESET}"
-./philo 2 800 200 200 10 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 10'
+./philo 2 800 200 200 10 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 < 10'
 
 echo "${BROWN}Test 02: 1 800 200 200${RESET}"
 ./philo 1 800 200 200 10 | grep died | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 1'
 
 echo "${BROWN}Test 03: 5 1000 200 200 7${RESET}"
-./philo 5 1000 200 200 7 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 7'
+./philo 5 1000 200 200 7 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 < 7'
 
 echo "${BROWN}Test 04: 1 800 200 200 200${RESET}"
 ./philo 1 800 200 200 10 | grep died | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 1'
 
 echo "${BROWN}Test 05: 5 800 200 200${RESET}"
-./philo 5 800 200 200 30 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 30'
+./philo 5 800 200 200 30 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 < 30'
 
 echo "${BROWN}Test 06: 5 800 200 200 7 ${RESET}"
-./philo 5 800 200 200 7 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 7'
+./philo 5 800 200 200 7 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 < 7'
 
 echo "${BROWN}Test 07: 4 410 200 200 200 ${RESET}"
-./philo 4 410 200 200 200 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 200'
+./philo 4 410 200 200 200 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 < 200'
 
 echo "${BROWN}Test 08: 4 310 200 100 ${RESET}"
 ./philo 4 310 200 100 | grep died | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 1'
 
 echo "${BROWN}Test 09: 199 800 200 200 200 ${RESET}"
-./philo 199 800 200 200 200 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 200'
+./philo 199 800 200 200 200 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 < 200'
 
 echo "${BROWN}Test 10: 2 500 250 250 ${RESET}"
 ./philo 2 500 250 250 | grep died | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 1'
@@ -55,19 +55,19 @@ echo "${BROWN}Test 12: 2 500 317 183 ${RESET}"
 ./philo 2 500 317 183 | grep died | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 1'
 
 echo "${BROWN}Test 13: 200 1000 200 200 200 ${RESET}"
-./philo 200 1000 200 200 200 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 200'
+./philo 200 1000 200 200 200 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 < 200'
 
 echo "${BROWN}Test 14: 200 1000 200 200 3 ${RESET}"
-./philo 200 1000 200 200 3 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 3'
+./philo 200 1000 200 200 3 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 < 3'
 
 echo "${BROWN}Test 15: 2 1000 200 200 50 ${RESET}"
-./philo 2 1000 200 200 50 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 50'
+./philo 2 1000 200 200 50 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 < 50'
 
 echo "${BROWN}Test 16: 5 1000 200 200 7 ${RESET}"
-./philo 5 1000 200 200 7 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 7'
+./philo 5 1000 200 200 7 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 < 7'
 
 echo "${BROWN}Test 17: 4 610 200 100 10 ${RESET}"
-./philo  4 610 200 100 10 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 10'
+./philo  4 610 200 100 10 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 < 10'
 
 echo "${BROWN}Test 18: 5 1000 200 200 30 ${RESET}"
-./philo 5 1000 200 200 30 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 != 30'
+./philo 5 1000 200 200 30 | grep eating | cut -d' ' -f2 | sort | uniq -c | run_awk_check '$1 < 30'
